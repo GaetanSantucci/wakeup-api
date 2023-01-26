7b1108db95e80b74e24b2c58ecee19aa881eb1b3
GaetanSantucci <gaetan.santucci@outlook.com>
2023-01-26 23:10:56 +0100

    :boom: add config express session and cors in index

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,50 @@
-// ~ ENVIRONMENT CONFIG  ~ //
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+// ~ Environment
 import 'dotenv/config';
 
-// ~ DEBUG CONFIG ~ //
+// ~ Debug
 
 import debug from 'debug';
 const logger = debug('Entrypoint');
 
-// ~ EXPRESS CONFIG ~ //
+// ~ Express
 import express from 'express';
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+import { Request, Response } from 'express';
+//~ Cors
+app.use((req: Request, res: Response, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  next();
+});
+//If you have your node.js behind a proxy and are using secure: true, you need to set 'trust proxy' in express
+app.set('trust proxy', 1);
+
+//~ Session
+import session from 'express-session';
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: process.env.SECRET_SESSION!,
+  cookie: {
+    secure: true,
+    maxAge: 1 * 60 * 60 * 1000, //1 hours
+    httpOnly: true,
+    // sameSite: 'none' //'lax', // or 'strict'
+    //expires : new Date(Date.now() + 60 * 60 * 1000) //1 hour
+  }
+}));
+
 import { router } from './app/router/index.js';
 // import { _404 } from './app/service/errorHandling.js';
 
-// ~ LAUNCHER CONFIG ~ //
+// ~ Launch router
 app.use('/api/v1', router);
 // app.use(_404)
 
